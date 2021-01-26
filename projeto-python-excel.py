db03d8f0d73d1a19779fd85664c6e12c95410b30
satohideki <hidekii@hotmail.co.jp>
2021-01-26 13:07:42 -0300

    pivot

--- projeto-python-excel.py
+++ projeto-python-excel.py
@@ -1,6 +1,16 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from io import BytesIO
+import os
+
+arquivo = input('Caminho: ')
+
+
 
 excel_file = (r'C:\Users\bruno.sato\my-first-project\python\awrtimebreakdown102.xlsx')
 df = pd.read_excel(excel_file)
-print (df.loc[:,'CPU'])
+evento_group = df.groupby(['DataHora', 'Evento']).mean()
+print(evento_group['Tempo(s)'])
+
+
 

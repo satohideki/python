e9d0fc8d38f35f6e0b1c5875b373101eb8d7d02b
satohideki <hidekii@hotmail.co.jp>
2021-02-01 12:58:25 -0300

    pequenas mudancas

--- projeto-python-excel.py
+++ projeto-python-excel.py
@@ -4,13 +4,14 @@ from io import BytesIO
 import os
 
 arquivo = input('Caminho: ')
+excel = (arquivo)
+df = pd.read_excel(excel)
+coluna = input('Qual coluna será a coluna? ')
+linha = input('Qual coluna será a linha? ')
+valor = input('Qual coluna será valores? ')
 
-
-
-excel_file = (r'C:\Users\bruno.sato\my-first-project\python\awrtimebreakdown102.xlsx')
-df = pd.read_excel(excel_file)
-evento_group = df.groupby(['DataHora', 'Evento']).mean()
-print(evento_group['Tempo(s)'])
+evento_group = df.groupby([linha, coluna]).mean()
+print(evento_group[valor])
 
 
 
